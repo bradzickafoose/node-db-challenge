c346c77ff870d6093f1eb9dabec55e14d8c5354e
Brad <brad@viziworx.com>
2019-12-13 12:55:43 -0500

    setup up and down functions for resources

--- routers/project-router.js
+++ routers/project-router.js
@@ -1,5 +1,31 @@
 const express = require('express')
 
+const db = require('../models/project-model')
+
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    db.getProjects()
+        .then(project => {
+            res
+                .status(200)
+                .json(project.map(newProject => {
+                    if (newProject.completed === 0) {
+                        return {
+                            ...newProject,
+                            completed: false
+                        }
+                    } else {
+                        return {
+                            ...newProject,
+                            completed: true
+                        }
+                    }
+                }))
+        })
+        .catch(err => {
+            res.status(400).json({ errorMessage: `Unable to get projects` })
+        })
+})
 
 module.exports = router;
